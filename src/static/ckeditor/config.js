28007b900100a350780a96ce7607a51f576a9dc0
guanhao97 <guanhao3797@gmail.com>
2014-06-14 03:25:43 +0800

    Added new plugin and made changes to script
    Lots of things to be aware of here:
     - divarea cannot be in the same extraPlugins string as autogrow
     - Height must come before divarea

--- src/static/ckeditor/config.js
+++ src/static/ckeditor/config.js
@@ -29,13 +29,16 @@ CKEDITOR.editorConfig = function( config ) {
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
-
+	
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 	
+	// Autogrow to grow textarea to a certain extent
+	// Divarea to switch from using iframe to div
+	// Divarea promotes faster reloads
 	config.extraPlugins = 'autogrow';
 	
 	// Makes autogrow functional on startup
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -6,7 +6,15 @@
 // Initiates CKEditor
 var editor = CKEDITOR.replace('editor', {
     // Enter key in editor creates linebreak
-    enterMode: CKEDITOR.ENTER_BR
+    enterMode: CKEDITOR.ENTER_BR,
+    // Height has to be declared before divarea
+    // As divarea will just set its own default height
+    height: '300px',
+    // Somehow when you put autogrow and divarea together,
+    // divarea's height and properties will be affected
+    // divarea loads CKEditor in a 'div' instead of 'iframe'
+    // This makes the load faster without blinking effect
+    extraPlugins: 'divarea'
 });
 
 (function worker() {
@@ -37,7 +45,7 @@ var editor = CKEDITOR.replace('editor', {
     });
 
     // Runs function for certain amount of time
-    setTimeout(worker, 1000);
+    setTimeout(worker, 3000);
 
 })();
 
