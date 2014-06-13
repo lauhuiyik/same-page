a96342f2b76253173145f9be07d6f9814f432b89
guanhao97 <guanhao3797@gmail.com>
2014-06-14 01:28:18 +0800

    Rewrote the order of Ajax function calls
    This is the order of calls:
    1. Checks if editor is dirty, sends POST request, memcache.set the data
    2. Periodically sends GET request to get Json from memcache.get
    3. editor.setData the following cached data
    
    So far, it worked like how I expected, it's just that it refreshes way
    too frequently and the cursor moves to the start too much that it's
    impossible to type into it.

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,23 +1,43 @@
-/*);
+/*
  * Author: Maverick Chan
  * Project: Same Page
  */
 
+// Initiates CKEditor
 var editor = CKEDITOR.replace('editor', {
+    // Enter key in editor creates linebreak
     enterMode: CKEDITOR.ENTER_BR
 });
 
-(function worker(){
-    text = editor.getData();
-    $.ajax({
-        url: '/ajax',
-        type: 'POST',
-        data: {
-            doc: text
-        }
+(function worker() {
+
+    if ( editor.checkDirty() ) {
+
+        text = editor.getData();
+
+        $.ajax({
+            url: '/ajax',
+            type: 'POST',
+            data: {
+                doc: text
+            }
+        }).done(function() {
+            editor.resetDirty();
+        });
+
+    }
+
+    // Checks if text in editor is edited
+    var new_data;
+
+    $.getJSON('/ajax', function(data) {
+        new_data = data['data'];
+    }).success(function() {
+        editor.setData(new_data);
     });
 
     // Runs function for certain amount of time
-    setTimeout(worker, 300);
+    setTimeout(worker, 1000);
+
 })();
 
