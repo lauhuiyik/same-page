d2908f56099db572aa7c1cd1cbfc2d058adf1408
guanhao97 <guanhao3797@gmail.com>
2014-06-13 14:56:09 +0800

    Made site work with memcache
    This is still at the testing round where I will need it to go live so
    the changes can be seen.
    
    Expected problems:
     - No live change as script.js only runs Ajax calls when previous
       on-page data is different from current (meaning on user interaction).
     - This means that a change in the cache DOES NOT call Ajax GET request
       to change the value of the text editor.

--- src/handlers/ajax.py
+++ src/handlers/ajax.py
@@ -14,23 +14,12 @@ from base_handler import BaseHandler
 class AjaxHandler(BaseHandler):
     
     def get(self):
-        doc_name = self.request.cookies.get('doc') 
-        doc_dir = os.path.join(os.path.dirname(__file__),
-                               'tmp', doc_name)
-        with open(doc_dir, 'r') as f:
-            content = f.read()
-
-        return content
+        data = memcache.get('data')
+        return data
         
     def post(self):
         data = self.request.get('doc')
         log.info('New data: '+ data)
 
-        doc_name = self.request.cookies.get('doc')
-        doc_dir = os.path.join(os.path.dirname(__file__),
-                               'tmp', doc_name)
-        
-        f = open(doc_dir, 'w')
-        f.write(data)
-        f.close()
+        memcache.set('data', data)
 
