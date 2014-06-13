1158212e4dddc5252cb9f8395d96a7ce6fdce0ba
guanhao97 <guanhao3797@gmail.com>
2014-06-14 00:32:24 +0800

    Moved editor instance to script.js
    This is because the editor instance is really important to manipulate
    changes in the editor.
    
    I have also added the predefined settings what the Enter key can do.
    Now, it's defaulted to creating a linebreak. Which makes it less choppy.

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -3,42 +3,19 @@
  * Project: Same Page
  */
 
-$('textarea.editor').ckeditor();
-
-var prev_text = '';
-
-var range = editor.createRange();
-
-editor.getSelection().selectRanges( [ range ] );
+var editor = CKEDITOR.replace('editor', {
+    enterMode: CKEDITOR.ENTER_BR
+});
 
 (function worker(){
-    var text = $('textarea.editor').val();
-
-    function submit_change(old_data, current_data) {
-        // Only make POST request upon current user change
-        if (current_data != old_data) {
-            $.ajax({
-                url: '/ajax',
-                type: 'POST',
-                data: {
-                    doc: text
-                }
-            });
-        }
-    }
-
-    submit_change(prev_text, text)
-    
-    // Listen for everything
-    $.getJSON('/ajax', function(data) {
-        new_data = data['data'];
-        if (new_data != text) {
-            $('textarea.editor').val(new_data);
-            range.moveToElementEditablePosition( editor.editable(), true ); // bar.^</p>
+    text = editor.getData();
+    $.ajax({
+        url: '/ajax',
+        type: 'POST',
+        data: {
+            doc: text
         }
     });
-    
-    prev_text = text;
 
     // Runs function for certain amount of time
     setTimeout(worker, 300);
