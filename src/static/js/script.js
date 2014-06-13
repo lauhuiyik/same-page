c135e229faee5b3707237c296e31d7a921868ca2
guanhao97 <guanhao3797@gmail.com>
2014-06-13 20:49:42 +0800

    Changed Ajax to have multiple callbacks
    Previously, I used jQuery.when to wrap all the functions around and have
    only one callback.
    
    Now, I changed it because listening changes has to be
    done frequently while sending changes only happens when the user submit
    changes. So they have to be different callbacks.

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,31 +1,41 @@
-/*
+/*);
  * Author: Maverick Chan
  * Project: Same Page
  */
 
 $('textarea.editor').ckeditor();
 
+var prev_text = '';
+
 (function worker(){
     var text = $('textarea.editor').val();
+
+    function submit_change(old_data, current_data) {
+        // Only make POST request upon current user change
+        if (current_data != old_data) {
+            $.ajax({
+                url: '/ajax',
+                type: 'POST',
+                data: {
+                    doc: text
+                }
+            });
+        }
+    }
+
+    submit_change(prev_text, text)
     
-    $.when(
-
-        $.getJSON('/ajax', function(data) {
-            console.log(data)
-        }),
-
-        $.ajax({
-            url: '/ajax',
-            type: 'POST',
-            data: {
-                doc: text
-            }
-        })
-
-    ).done(function(){
-    
+    // Listen for everything
+    $.getJSON('/ajax', function(data) {
+        new_data = data['data'];
+        if (new_data != text) {
+            $('textarea.editor').val(new_data);
+        }
     });
+    
+    prev_text = text;
 
-    setTimeout(worker, 10000);
+    // Runs function for certain amount of time
+    setTimeout(worker, 300);
 })();
 
